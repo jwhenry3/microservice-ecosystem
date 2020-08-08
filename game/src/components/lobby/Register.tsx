efbe033612ab8ab1cd578f434029fa1b106106eb
Justin <jwhenry123@gmail.com>
2020-08-08 12:35:47 -0500

    removing layers of complexity

--- game/src/components/lobby/Register.tsx
+++ game/src/components/lobby/Register.tsx
@@ -3,9 +3,11 @@ import './Login.scss';
 import { Formik }            from 'formik';
 import { Button, TextField } from '@material-ui/core';
 
-export default class Login extends Component<any, any> {
 
-  validate = (values: { email: string, password: string }) => {
+export default class Register extends Component<any, any> {
+
+
+  validate = (values: { email: string, password: string, confirmPassword: string }) => {
     const errors: any = {};
     if (!values.email) {
       errors.email = 'Required';
@@ -15,20 +17,25 @@ export default class Login extends Component<any, any> {
     if (!values.password) {
       errors.password = 'Required';
     }
+    if (!values.confirmPassword) {
+      errors.confirmPassword = 'Required';
+    } else if (values.confirmPassword !== values.password) {
+      errors.confirmPassword = 'Password and Confirm Password must match';
+    }
     return errors;
   };
 
-  onSubmit = (values: { email: string, password: string }, { setSubmitting }) => {
+  onSubmit = (values: { email: string, password: string, confirmPassword: string }, { setSubmitting }) => {
     console.log('submit!');
     setSubmitting(false);
   };
 
   render() {
     return <div className="login">
-      <h2>Please Login</h2>
+      <h2>Register</h2>
       <div className="login-form">
         <Formik
-          initialValues={{ email: '', password: '' }}
+          initialValues={{ email: '', password: '', confirmPassword: '' }}
           validate={this.validate}
           onSubmit={this.onSubmit}>
           {({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
@@ -57,6 +64,19 @@ export default class Login extends Component<any, any> {
                   onBlur={handleBlur}
                   value={values.password}/>
                 <div>{errors.password && touched.password && errors.password}</div>
+              </div>
+              <br/>
+              <div className="field">
+                <TextField
+                  variant="outlined"
+                  autoComplete="off"
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.confirmPassword}/>
+                <div>{errors.confirmPassword && touched.confirmPassword && errors.confirmPassword}</div>
               </div>
               <br/>
               <Button variant="contained" color="primary" type="submit" disabled={isSubmitting}>
